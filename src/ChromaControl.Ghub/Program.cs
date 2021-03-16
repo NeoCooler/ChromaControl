4e9cf588ae81e1766a63da636706c6ed32a429e7
NeoCooler <neocooler29@gmail.com>
2021-03-16 22:10:30 +0100

    Initial Commit

---
+++ src/ChromaControl.Ghub/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Hosting;
+using RGBKit.Core;
+using ChromaControl.Shared;
+
+namespace ChromaControl.Ghub
+{
+    /// <summary>
+    /// The Chroma Control service program
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// The program entry point
+        /// </summary>
+        /// <param name="args">The command line arguments</param>
+        public static void Main(string[] args)
+        {
+            if (!Utilities.InitializeEnvironment("ChromaControl.Ghub", "00000000-0000-0000-0000-000000000000", args))
+                return;
+
+            Utilities.CreateDefaultHost(args)
+                .ConfigureRGBKit(rgbKit =>
+                {
+                    rgbKit.UseGhub();
+                })
+                .Build().Run();
+        }
+    }
+}
